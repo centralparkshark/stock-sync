203ce9b4b1d784dd9b951ba8094ae833592c8314
Vinncent Miller <vinncentmiller@gmail.com>
2024-10-08 09:18:46 -0400

    Reworked shopify model to allow for variants

--- backend/models/Shopify.js
+++ backend/models/Shopify.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const shopifySchema = new mongoose.Schema({
-    sku: { type: String, required: true},
+    sku: { type: String, required: true}, // default sku if only one item
     title: { type: String, required: true},
     description: { type: String }, 
     stock: { type: Number, default: 0},
@@ -14,7 +14,24 @@ const shopifySchema = new mongoose.Schema({
     vendor: {type: String, default: "Heinz History Center"}, 
     collections: {type: [String]},
     status: {enum: ["draft", "active", "archived"], default: "draft", required: true},
-    lastUpdated: {type: String, required: true},
+    lastUpdated: {type: String, required: true}, // date
+    variants: [
+        {
+            sku: { type: String }, // variant sku
+            stock: { type: Number, default: 0},
+            price:{ type: Number },
+            compareAtPrice: { type: Number },
+            weight: { type: Number },
+            weightType: { type: String },
+
+            options: [
+                {
+                    name: { type: String }, // ex. size, color
+                    value: { type: String }, //e ex. red, Large
+                }
+            ]
+        }
+    ]
 })
 
 // required is sku, title, status, and last updated
