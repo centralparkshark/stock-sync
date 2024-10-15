8836460ccd6fe2a0f28e09bdadf980019f96f23f
unknown <vinncentmiller@gmail.com>
2024-10-15 11:54:56 -0400

    basic export page

---
+++ frontend/src/pages/ExportPage.jsx
@@ -0,0 +1,8 @@
+export default function ExportPage() {
+    return (
+        <div className="card">
+            <h2>Export Items for Shopify:</h2>
+            <button>Download CSV File</button>
+        </div>
+    )
+}
