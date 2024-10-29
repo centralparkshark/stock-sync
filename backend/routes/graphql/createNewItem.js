3b80c87e53fd596f07fb34feebe13237a2afd118
Vinncent Miller <vinncentmiller@gmail.com>
2024-10-29 10:55:25 -0400

    Sets inventory as tracked and changes it to 10% of tam value

--- backend/routes/graphql/createNewItem.js
+++ backend/routes/graphql/createNewItem.js
@@ -3,8 +3,8 @@ import checkIfExists from "./checkIfExists.js";
 
 export default async function createNewItem(req, res) {
     const productData = req.body;
-
-    console.log(productData)
+    const location = "gid://shopify/Location/96155828471" // will need changed
+    const qty = Math.floor(productData.stock * 0.1)
 
     try {
         if (await checkIfExists(productData.sku)) {
@@ -14,27 +14,42 @@ export default async function createNewItem(req, res) {
         // Create a new product variant
         const mutation = `mutation {
             productSet(input: {
-                title: "${productData.title}"
-                descriptionHtml: "${productData.description}"
+                title: "${productData.title}",
+                descriptionHtml: "${productData.description}",
                 status: DRAFT,
                 tags: "testTag1, testTag2",
                 vendor: "${productData.vendor}",
                 productOptions: [{
                     name: "Title",
                     values: [{name: "Default Title"}]
-                }]
+                }],
                 variants: [
                     {
                         
                         price: ${productData.price},
                         sku: "${productData.sku}",
-                        optionValues: [{optionName: "Title", name: "Default Title"}]
+                        optionValues: [{optionName: "Title", name: "Default Title"}],
+                        inventoryQuantities: [{
+                            name: "available",
+                            locationId: "${location}",
+                            quantity: ${qty}
+                        }]
                     }
                 ]
             }) {
                 product {
                     id
                     title
+                    variants(first: 1) {
+                        edges {
+                            node {
+                                id
+                                inventoryItem {
+                                    id
+                                }
+                            }
+                        }
+                    }
                 }
                 userErrors {
                     field
@@ -57,11 +72,43 @@ export default async function createNewItem(req, res) {
         );
 
 
-        // console.log(JSON.stringify(createResponse.data, null, 2));
         const { product, userErrors } = createResponse.data.data.productSet;
         if (userErrors.length > 0) {
             return res.status(400).json({ errors: userErrors });
         }
+
+        // set inventory management to shopify
+        const trackedMutation = `
+            mutation {
+            inventoryItemUpdate(id: "${product.variants.edges[0].node.inventoryItem.id}", input: { tracked: true }) {
+                inventoryItem {
+                id
+                tracked
+                }
+                userErrors {
+                field
+                message
+                }
+            }
+            }
+        `
+        const createTrackedResponse = await axios.post(
+            `https://${process.env.SHOP}.myshopify.com/admin/api/2024-10/graphql.json`,
+            { query: trackedMutation },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Shopify-Access-Token': process.env.ADMIN_ACCESS_TOKEN,
+                },
+            }
+        );
+        
+        console.log(createTrackedResponse.data)
+        // const { product, userErrors } = createTrackedResponse.data.data.productSet;
+        // if (userErrors.length > 0) {
+        //     return res.status(400).json({ errors: userErrors });
+        // }
+
         res.status(201).json(product);
     } catch (e) {
         console.error(e);
